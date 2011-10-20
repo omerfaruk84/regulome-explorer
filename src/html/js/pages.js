562e4d131add9db86b17e770f012d78a4a4a7dbe
Hector Rovira <hrovira.isb@gmail.com>
2011-10-20 11:10:55 -0700

    fixed logging

--- src/html/js/pages.js
+++ src/html/js/pages.js
@@ -3,16 +3,16 @@ Ext.ns('org.systemsbiology.pages.apis.containers');
 org.systemsbiology.pages.apis.containers.Containers = [];
 
 org.systemsbiology.pages.apis.containers.Register = function(container) {
-    console.log("Register(" + container + ")");
+    console.log("org.systemsbiology.pages.apis.containers.Register(" + container + ")");
     org.systemsbiology.pages.apis.containers.Containers.push(container);
     eval(container + "({})");
 };
 
 org.systemsbiology.pages.apis.containers.Bootstrap = function(jsonConfig) {
-    console.log("Bootstrap(" + jsonConfig + ")");
+    console.log("org.systemsbiology.pages.apis.containers.Bootstrap(" + jsonConfig + ")");
     if (jsonConfig.scripts) {
         Ext.each(jsonConfig.scripts, function(script) {
-            console.log("Bootstrap(): dom+" + script);
+            console.log("org.systemsbiology.pages.apis.containers.Bootstrap(): dom.head+" + script);
             Ext.DomHelper.append(Ext.getHead(), { tag: 'script', src: script,  type: "text/javascript" });
         });
     }
