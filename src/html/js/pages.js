5901e4674df333da23876f4ac85a5818f107c809
Hector Rovira <hrovira.isb@gmail.com>
2011-10-20 10:57:04 -0700

    adding bootstrap

--- src/html/js/pages.js
+++ src/html/js/pages.js
@@ -5,4 +5,15 @@ org.systemsbiology.pages.apis.containers.Containers = [];
 org.systemsbiology.pages.apis.containers.Register = function(container) {
     console.log("Register(" + container + ")");
     org.systemsbiology.pages.apis.containers.Containers.push(container);
+    eval(container + "({})");
+};
+
+org.systemsbiology.pages.apis.containers.Bootstrap = function(jsonConfig) {
+    console.log("Bootstrap(" + jsonConfig + ")");
+    if (jsonConfig.scripts) {
+        Ext.each(jsonConfig.scripts, function(script) {
+            console.log("Bootstrap(): dom+" + script);
+            Ext.DomHelper.append(Ext.getHead(), { tag: 'script', src: script,  type: "text/javascript" });
+        });
+    }
 };
