edd5406ea3ae63d73727bed9632fffd3f7a4503d
Hector Rovira <hrovira.isb@gmail.com>
2011-10-20 12:03:25 -0700

    changing bootstrap mechanism

--- src/html/js/pages.js
+++ src/html/js/pages.js
@@ -1,19 +1,24 @@
 Ext.ns('org.systemsbiology.pages.apis.containers');
 
-org.systemsbiology.pages.apis.containers.Containers = [];
-
-org.systemsbiology.pages.apis.containers.Register = function(container) {
-    console.log("org.systemsbiology.pages.apis.containers.Register(" + container + ")");
-    org.systemsbiology.pages.apis.containers.Containers.push(container);
-    eval(container + "({})");
-};
+org.systemsbiology.pages.apis.containers.Scripts = [];
 
 org.systemsbiology.pages.apis.containers.Bootstrap = function(jsonConfig) {
     console.log("org.systemsbiology.pages.apis.containers.Bootstrap(" + jsonConfig + ")");
     if (jsonConfig.scripts) {
+        // TODO : Filter scripts that have already been loaded
         Ext.each(jsonConfig.scripts, function(script) {
             console.log("org.systemsbiology.pages.apis.containers.Bootstrap(): dom.head+" + script);
+            org.systemsbiology.pages.apis.containers.Scripts.push(script);
             Ext.DomHelper.append(Ext.getHead(), { tag: 'script', src: script,  type: "text/javascript" });
         });
+    }
+
+    if (jsonConfig.containers) {
+        Ext.each(jsonConfig.containers, function(container) {
+            console.log("org.systemsbiology.pages.apis.containers.Bootstrap(): containers+" + container);
+            org.systemsbiology.pages.apis.containers.Containers.push(container);
+            data = "{}";
+            eval(container + "(" + data + ")");
+        })
     }
 };
