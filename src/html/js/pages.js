1fdf7f6ca82d715cec31839314169d85bfe1f0e8
Hector Rovira <hrovira.isb@gmail.com>
2011-10-20 10:24:00 -0700

    testing api

---
+++ src/html/js/pages.js
@@ -0,0 +1,12 @@
+Ext.ns('org.systemsbiology.pages.apis.containers');
+
+(function() {
+    console.log("pages.js");
+    var Containers = [];
+
+    var Register = function(container) {
+        console.log("Register(" + container + ")");
+        Containers.push(container);
+    };
+})();
+
