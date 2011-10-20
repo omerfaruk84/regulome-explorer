0780759fc715342b682537d556e5b6d9003743b7
Hector Rovira <hrovira.isb@gmail.com>
2011-10-20 12:16:02 -0700

    changed bootstrap to load separately

--- src/html/js/pages.js
+++ src/html/js/pages.js
@@ -2,21 +2,23 @@ Ext.ns('org.systemsbiology.pages.apis.containers');
 
 org.systemsbiology.pages.apis.containers.Scripts = [];
 
-org.systemsbiology.pages.apis.containers.Bootstrap = function(jsonConfig) {
-    console.log("org.systemsbiology.pages.apis.containers.Bootstrap(" + jsonConfig + ")");
-    if (jsonConfig.scripts) {
+org.systemsbiology.pages.apis.containers.Bootstrap = function(scripts) {
+    console.log("org.systemsbiology.pages.apis.containers.Bootstrap(" + scripts + ")");
+    if (scripts) {
         // TODO : Filter scripts that have already been loaded
-        Ext.each(jsonConfig.scripts, function(script) {
-            console.log("org.systemsbiology.pages.apis.containers.Bootstrap(): dom.head+" + script);
+        Ext.each(scripts, function(script) {
+            console.log("org.systemsbiology.pages.apis.containers.Bootstrap(): script=" + script);
             org.systemsbiology.pages.apis.containers.Scripts.push(script);
             Ext.DomHelper.append(Ext.getHead(), { tag: 'script', src: script,  type: "text/javascript" });
         });
     }
+};
 
-    if (jsonConfig.containers) {
-        Ext.each(jsonConfig.containers, function(container) {
-            console.log("org.systemsbiology.pages.apis.containers.Bootstrap(): containers+" + container);
-            org.systemsbiology.pages.apis.containers.Containers.push(container);
+org.systemsbiology.pages.apis.containers.Load = function(containers, jsonData) {
+    console.log("org.systemsbiology.pages.apis.containers.Load(" + containers + ")");
+    if (containers) {
+        Ext.each(containers, function(container) {
+            console.log("org.systemsbiology.pages.apis.containers.Load(): container=" + container);
             data = "{}";
             eval(container + "(" + data + ")");
         })
