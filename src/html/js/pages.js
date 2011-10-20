d4dadd1d170657db17f4f21c78d6a752c9f29aec
Hector Rovira <hrovira.isb@gmail.com>
2011-10-20 13:01:55 -0700

    trying different track for eventing

--- src/html/js/pages.js
+++ src/html/js/pages.js
@@ -2,15 +2,44 @@ Ext.ns('org.systemsbiology.pages.apis.containers');
 
 org.systemsbiology.pages.apis.containers.Scripts = [];
 
-org.systemsbiology.pages.apis.containers.Bootstrap = function(scripts) {
+org.systemsbiology.pages.apis.containers.BootstrapListener = {
+    _internal : [],
+
+    constructor: function(scripts, callback) {
+        console.log("org.systemsbiology.pages.apis.containers.BootstrapListener(" + scripts + "," + callback + ")");
+        Ext.each(scripts, function(script) {
+            _internal.push(script);
+        });
+        Ext.apply(this, { scripts: _internal, callback: callback});
+    },
+    
+    Listen: function(event, t, o) {
+        console.log("org.systemsbiology.pages.apis.containers.BootstrapListener.Listen(" + event + "," + t + "," + o + ")");
+        for (var i = 0; i < _internal.length; i++) {
+            if (this._internal[i] == o) {
+                this._internal.splice(i, 1);
+            }
+        }
+        if (this._internal.length == 0) {
+            this.callback();
+        }
+    }
+};
+
+org.systemsbiology.pages.apis.containers.Bootstrap = function(scripts, callback) {
     console.log("org.systemsbiology.pages.apis.containers.Bootstrap(" + scripts + ")");
     if (scripts) {
+        var listener = org.systemsbiology.pages.apis.containers.BootstrapListener(scripts, callback);
+
         // TODO : Filter scripts that have already been loaded
         Ext.each(scripts, function(script) {
             console.log("org.systemsbiology.pages.apis.containers.Bootstrap(): script=" + script);
             org.systemsbiology.pages.apis.containers.Scripts.push(script);
-            Ext.DomHelper.append(Ext.getHead(), { tag: 'script', src: script,  type: "text/javascript" });
+            var elem = Ext.DomHelper.append(Ext.getHead(), { tag: 'script', src: script,  type: "text/javascript" }, true);
+            elem.on("load", listener.Listen, listener, script);
         });
+    } else {
+        callback();
     }
 };
 
@@ -20,7 +49,7 @@ org.systemsbiology.pages.apis.containers.Load = function(containers, jsonData) {
         Ext.each(containers, function(container) {
             console.log("org.systemsbiology.pages.apis.containers.Load(): container=" + container);
             data = "{}";
-            eval(container + "(" + data + ")");
+            // eval(container + "(" + data + ")");
         })
     }
 };
