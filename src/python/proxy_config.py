f680a5ff41df66cbdc9782cb970df4d885f64755
Hector Rovira <hrovira.isb@gmail.com>
2011-10-21 15:15:41 -0700

    added proxy for cytoscapeweb

--- src/python/proxy_config.py
+++ src/python/proxy_config.py
@@ -8,5 +8,6 @@ mappings = {
     "/solr/.*": {"host": "http://vis.systemsbiology.net"},
     "/carambola/.*": {"host": "https://cancerregulome.googlecode.com/hg"},
     "/codefordemos/.*": {"host": "https://codeforsystemsbiology.googlecode.com/hg"},
+    "/cytoscapeweb_v0.8/.*": {"host": "http://informatics-apps.systemsbiology.net"},
     "/files/.*": {"host": "https://codeforsystemsbiology.googlecode.com"}
 }
