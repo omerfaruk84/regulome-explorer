607135ec8bd3ecb14aa288dc2d364defa5769783
hrovira <hrovira@systemsbiology.org>
2011-10-21 21:07:24 +0000

    added carambola proxy mapping

--- src/python/proxy_config.py
+++ src/python/proxy_config.py
@@ -3,5 +3,8 @@
 # mappings = {"/data-api/.*": {"host": "http://datahost/api"},
 #             "/html/.*": {"host": "http://content.server/pages"}}
 
-mappings = {"/google-dsapi-svc/.*": {"host": "http://vis.systemsbiology.net"},
-            "/solr/.*": {"host": "http://vis.systemsbiology.net"}}
+mappings = {
+    "/google-dsapi-svc/.*": {"host": "http://vis.systemsbiology.net"},
+    "/solr/.*": {"host": "http://vis.systemsbiology.net"},
+    "/carambola/.*": {"host": "https://cancerregulome.googlecode.com/hg/carambola/src"}
+}
