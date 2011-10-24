3872e4b1a3c50a6b7605ac927559561069b7a873
Hector Rovira <hrovira.isb@gmail.com>
2011-10-24 11:09:08 -0700

    got script load to work

--- src/html/js/scriptLoad.js
+++ src/html/js/scriptLoad.js
@@ -5,14 +5,15 @@ org.systemsbiology.pages.util.Scripts = [];
 org.systemsbiology.pages.util.ScriptLoad = function(urls) {
     Ext.each(urls, function(url) {
         if (org.systemsbiology.pages.util.Scripts.indexOf(url) == -1) {
-            Ext.Ajax.request({
-                url: url,
-                method: "GET",
-                success: function(o) {
-                    org.systemsbiology.pages.util.Scripts.push(url);
-                    eval(o.responseText);
-                }
-            });
+            if (document.createElement && document.childNodes) {
+                var scriptElem = document.createElement('script');
+                scriptElem.setAttribute('src', url);
+                scriptElem.setAttribute('type', 'text/javascript');
+                document.getElementsByTagName('head')[0].appendChild(scriptElem);
+                org.systemsbiology.pages.util.Scripts.push(url);
+            } else {
+                alert('Your browser is not W3C DOM compliant.');
+            }
         }
     });
 };
