d6aca64a8e6eed0c1b3cb44d7d4f13aaeaad941f
Hector Rovira <hrovira.isb@gmail.com>
2011-10-24 11:09:08 -0700

    got script load to work

---
+++ src/html/js/pages.js
@@ -0,0 +1,56 @@
+Ext.ns('org.systemsbiology.pages.apis.containers');
+
+var StringToFunction = function(str) {
+    var fn = (window || this);
+    Ext.each(str.split("."), function(a) {
+        if (a && fn[a]) {
+            fn = fn[a];
+        }
+    });
+
+    if (typeof fn !== "function") {
+        console.log("unable to load: " + str);
+    }
+
+    return  fn;
+};
+
+org.systemsbiology.pages.util.Callbacks = {};
+
+org.systemsbiology.pages.apis.containers.OnReady = function(container, callback) {
+    var vals = org.systemsbiology.pages.util.Callbacks[container];
+    if (vals == null) {
+        vals = new Array();
+        org.systemsbiology.pages.util.Callbacks[container] = vals;
+    }
+    vals.push(callback);
+};
+
+org.systemsbiology.pages.apis.containers.IsReady = function(container) {
+    var vals = org.systemsbiology.pages.util.Callbacks[container];
+    if (vals != null) {
+        Ext.each(vals, function(cb) {
+            cb();
+        });
+    }
+};
+
+org.systemsbiology.pages.apis.containers.Bootstrap = function(scripts) {
+    console.log("org.systemsbiology.pages.apis.containers.Bootstrap(" + scripts + ")");
+    if (scripts && scripts.length) {
+        org.systemsbiology.pages.util.ScriptLoad(scripts);
+    }
+};
+
+org.systemsbiology.pages.apis.containers.Load = function(parentDiv, container, data, options) {
+    console.log("org.systemsbiology.pages.apis.containers.Load(" + parentDiv + "," + container + ")");
+    if (parentDiv && container) {
+        var childDiv = Ext.DomHelper.append(parentDiv, { tag: 'div' }, true);
+
+        var ContainerClass = StringToFunction(container);
+        if (ContainerClass) {
+            var containerInstance = new ContainerClass(childDiv);
+            containerInstance.draw(data, options);
+        }
+    }
+};
---
+++ src/python/proxy_config.py
@@ -0,0 +1,13 @@
+# Example:
+#
+# mappings = {"/data-api/.*": {"host": "http://datahost/api"},
+#             "/html/.*": {"host": "http://content.server/pages"}}
+
+mappings = {
+    "/google-dsapi-svc/.*": {"host": "http://vis.systemsbiology.net"},
+    "/solr/.*": {"host": "http://vis.systemsbiology.net"},
+    "/carambola/.*": {"host": "https://cancerregulome.googlecode.com/hg"},
+    "/codefordemos/.*": {"host": "https://codeforsystemsbiology.googlecode.com/hg"},
+    "/cytoscapeweb_v0.8/.*": {"host": "http://informatics-apps.systemsbiology.net"},
+    "/files/.*": {"host": "https://codeforsystemsbiology.googlecode.com"}
+}
