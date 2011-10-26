8950981b9b280bd7ac88250e48cabc76f6054591
Hector Rovira <hrovira.isb@gmail.com>
2011-10-26 14:13:50 -0700

    cleanup

--- src/html/js/scriptLoad.js
+++ src/html/js/scriptLoad.js
@@ -2,18 +2,16 @@ Ext.namespace('org.systemsbiology.pages.util');
 
 org.systemsbiology.pages.util.Scripts = [];
 
-org.systemsbiology.pages.util.ScriptLoad = function(urls) {
-    Ext.each(urls, function(url) {
-        if (org.systemsbiology.pages.util.Scripts.indexOf(url) == -1) {
-            if (document.createElement && document.childNodes) {
-                var scriptElem = document.createElement('script');
-                scriptElem.setAttribute('src', url);
-                scriptElem.setAttribute('type', 'text/javascript');
-                document.getElementsByTagName('head')[0].appendChild(scriptElem);
-                org.systemsbiology.pages.util.Scripts.push(url);
-            } else {
-                alert('Your browser is not W3C DOM compliant.');
-            }
+org.systemsbiology.pages.util.ScriptLoad = function(url) {
+    if (org.systemsbiology.pages.util.Scripts.indexOf(url) == -1) {
+        if (document.createElement && document.childNodes) {
+            var scriptElem = document.createElement('script');
+            scriptElem.setAttribute('src', url);
+            scriptElem.setAttribute('type', 'text/javascript');
+            document.getElementsByTagName('head')[0].appendChild(scriptElem);
+            org.systemsbiology.pages.util.Scripts.push(url);
+        } else {
+            alert('Your browser is not W3C DOM compliant.');
         }
-    });
+    }
 };
