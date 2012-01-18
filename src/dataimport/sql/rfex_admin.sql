20161dbc9e64ff05f240f50b7c86f30781f06ea4
jlin <jlin@systemsbiology.org>
2012-01-18 17:50:29 +0000

    need this for import

--- src/dataimport/sql/rfex_admin.sql
+++ src/dataimport/sql/rfex_admin.sql
@@ -14,3 +14,16 @@ CREATE TABLE `tcga`.`regulome_explorer_dataset`
 
 CREATE UNIQUE INDEX PRIMARY ON regulome_explorer_dataset(label);
 
+ 
+CREATE TABLE `sample_meta` (
+  `sample_key` varchar(20) NOT NULL default '',
+  `cancer_type` varchar(20) NOT NULL default '',
+  `dataset_label` varchar(50) NOT NULL default '',
+  `matrix_col_offset` int(11) default NULL,
+  `meta_json` text)
+
+ALTER TABLE 'sample_meta' ADD CONSTRAINT pk_sample_meta  PRIMARY KEY  (`sample_key`,`cancer_type`,`dataset_label`);
+
+
+
+
