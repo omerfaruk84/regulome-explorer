68915a5c61e9fed675545ebe5db4f26a17033ceb
Dick Kreisberg <rkreisberg@systemsbiology.org>
2012-02-22 16:40:17 -0800

    updated tooltip items/links parsing and functionality.  branches on feature.source.  got rid of unnecessary menu items

--- src/webapp/js/utils.js
+++ src/webapp/js/utils.js
@@ -48,16 +48,17 @@ function parseLabel(label) {
 
 function parseAnnotationList(feature) {
     var list =[];
-    var annotations = '';
+    var annotations = null;
     if (feature.source == 'GNAB') {
         list = feature.label_mod.split('_');
-        annotations = (list[0] == 'dom' ? list[1] + '-' + list[2] : '');
+        annotations = (list[0] == 'dom' ? 'domain: '+ list[1] + '-' + list[2] : '');
         list = ( annotations == '' ? list : list.slice(3));
 //        annotations = annotations + (list.length > 0 ? list.join(', ') :'any');
         annotations = annotations +  list.map(translateGNABAnnotation).filter(function(a) { return a != '';}).join(', ');
     }
 
     if (feature.source == 'CNVR') {
+
             list = feature.label_mod.split('_');
             annotations = list.map(translateCNVRAnnotation).join(', ');
         }
